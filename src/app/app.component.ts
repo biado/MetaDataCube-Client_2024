dab954f719c0d5dc0caf31eab7c29efc0bfb828d
pierre <pierre.ouvrard79@orange.fr>
2024-05-27 17:10:40 +0000

    Graphic style of the main component implemented #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Inject, OnInit, PLATFORM_ID, HostListener } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 interface Element {
   id: number;
@@ -9,59 +9,36 @@ interface Element {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
+export class AppComponent implements OnInit {
+  
+  display_dimensions: boolean = true;
+  display_filters: boolean = true;
+  smallscreen: boolean = false;
 
-export class AppComponent {
-  title = 'PhotoCube-Client_2024';
-
-  nodes = 8960
-  y=27
-
-  constructor(
-  ) { }
-
-  elements: Element[] = [];
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngOnInit(): void {
-    const names: string[] = [
-      "YYield",
-      "!exclamation",
-      "#hashtag",
-      "aLowercase",
-      "bBase",
-      "ZUppercase",
-      "$dollar",
-      "cri",
-      "1number",
-      "2digits",
-      "BBBBB",
-      "3three",
-      "Athree",
-      "(85("
-    ];
-
-    // Transforme la liste de noms en une liste d'objets avec id et name
-    this.elements = names.map((name, index) => ({
-      id: index + 1,
-      name: name
-    }));
-
-    console.log('Before sorting:', this.elements);
-
-    // Trie la liste par nom
-    this.elements.sort((a, b) => a.name.localeCompare(b.name));
-
-    console.log('After sorting:', this.elements);
-
-    const number = 8
-
-    console.log(this.elements.find(element => element.id === number)?.name)
-   
+    if (isPlatformBrowser(this.platformId)) {
+      this.smallscreen = window.innerWidth < 1101;
+    }
   }
 
-
-  getnodes() : void{
-    console.log('hello')
+  display_dimensions_change(): void {
+    this.display_dimensions = !this.display_dimensions;
   }
+
+  display_filters_change(): void {
+    this.display_filters = !this.display_filters;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.smallscreen = window.innerWidth < 1101;
+      console.log("Valeur :",this.smallscreen)
+    }
+  }
+  
 }
